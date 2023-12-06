9cfc6ece4141aa80e92e8a20016ed7c6ad018793
Niall BURDON <niall.burdon@rubrash.com>
2023-12-06 22:39:14 +0100

    set up project

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Layout</h2>
+    </div>
+
+    <!-- output the page content -->
+    <div>
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+body{
+    background-color: darkblue;
+}
+h2{
+    color: greenyellow;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptates non unde vel placeat doloribus, vitae molestias quis officia. Corporis!</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>

701d6b88c655d39a6338a42a54e9dc1a59b4fee7
Niall BURDON <niall.burdon@rubrash.com>
2023-12-07 10:25:15 +0100

    sass functioning

--- pages/about.vue
+++ pages/about.vue
@@ -14,6 +14,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+h1{
+    color: $primary;
+}
 </style>
---
+++ pages/services/[id].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        il
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1{
+        color: lightblue;
+    }
+</style>

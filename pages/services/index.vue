6ad53c3c17758537490478e48f6de1d71acdc4a0
Niall BURDON <niall.burdon@rubrash.com>
2023-12-09 18:12:07 +0100

    added reusable components

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -2,18 +2,20 @@
     <div>
         <h2>Services</h2>
         <div
-            v-for="(p, index) in products" :key="index">
-            <NuxtLink :to="`/services/${p.id}`">{{ p.title }}</NuxtLink>
+            v-for="(s, index) in services" :key="index">
+           <ServiceCard :service="s"/>
+           <!-- <NuxtLink :to="`/services/${s.id}`">{{ s.title }}</NuxtLink> -->
         </div>
     </div>
 </template>
 
 <script setup>
+    //layout
     definePageMeta({
        layout:'services'
     })
     //fetch products
-    const {data:products} = await useFetch('https://fakestoreapi.com/products')
+    const {data:services} = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <style lang="scss" scoped>

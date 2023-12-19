b13f9ef3a2bc0321510cd4b18e71fa24dce0b796
Niall BURDON <niall.burdon@rubrash.com>
2023-12-19 16:46:46 +0100

    merge

---
+++ components/HeaderElement.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Header
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

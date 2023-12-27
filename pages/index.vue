94e790f72484dae811d74675e85cab605b95528c
Niall BURDON <niall.burdon@rubrash.com>
2023-12-27 18:48:28 +0100

    grids and containers

--- pages/index.vue
+++ pages/index.vue
@@ -26,9 +26,33 @@
         </p>
         <p class="readmore">Lire plus</p>
       </div>
+      <div class="container large">
+        <h1 class="title-main title-purple">coachkeepers</h1>
+        <h1 class="title-secondary title-info">
+          une methodologie de travail fruit de 20 ans d’experience
+        </h1>
+
+        <h2 class="contact">Nous contacter</h2>
+        <h2>Nos objectifs</h2>
+        <h3>Menu</h3>
+        <blockquote>
+          CoachKeepers propose des services d’audit, sur mesure pour les
+          structures souhaitant gagner en expertise et créer de la valeur dans
+          le domaine des gardiens de but.
+        </blockquote>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at,
+          molestias nemo animi dolorem dignissimos, omnis iure alias saepe
+          quidem illum illo tempora assumenda facere voluptatibus eius iusto
+          quo? Iste!
+        </p>
+        <p class="readmore">Lire plus</p>
+      </div>
     </section>
-    <h1>Font sizes</h1>
-    <section></section>
+    <h1>Pics</h1>
+    <section>
+      <div class="container"></div>
+    </section>
   </div>
 </template>
 

aa509fed05d52503a987d1af34440a365a5aa9ee
Niall BURDON <niall.burdon@rubrash.com>
2023-12-27 13:28:55 +0100

    fonts

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-<h1>blabla</h1>
-    </div>
+  <div>
+    <h1>blabla</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
-
-        return {}
-    }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss">
-h1{
-    color: $primary;
+h1 {
+  color: inherit;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,35 @@
 <template>
-    <div>
+  <div>
+    <h2>Home</h2>
 
-<h2>Home</h2>
+    <h1>Font sizes</h1>
+    <section>
+      <div class="container">
+        <h1 class="title-main title-purple">coachkeepers</h1>
+        <h1 class="title-secondary title-info">
+          une methodologie de travail fruit de 20 ans d’experience
+        </h1>
 
-<h1>Section containers</h1>
-<section>
-
-    
-    <div class="container">
-        <font-awesome-icon icon="fa-brands fa-twitter-square" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptates non unde vel placeat doloribus, vitae molestias quis officia. Corporis!</p>
-    </div>
-    <div class="container large">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptates non unde vel placeat doloribus, vitae molestias quis officia. Corporis!</p>
-    </div>
-</section>
-
-
-    </div>
+        <h2 class="contact">Nous contacter</h2>
+        <h2>Nos objectifs</h2>
+        <h3>Menu</h3>
+        <blockquote>
+          CoachKeepers propose des services d’audit, sur mesure pour les
+          structures souhaitant gagner en expertise et créer de la valeur dans
+          le domaine des gardiens de but.
+        </blockquote>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at,
+          molestias nemo animi dolorem dignissimos, omnis iure alias saepe
+          quidem illum illo tempora assumenda facere voluptatibus eius iusto
+          quo? Iste!
+        </p>
+        <p class="readmore">Lire plus</p>
+      </div>
+    </section>
+    <h1>Font sizes</h1>
+    <section></section>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>

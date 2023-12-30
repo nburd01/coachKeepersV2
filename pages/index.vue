b1cca3f8cec43834a20ef53096c26b58ab874b74
Niall BURDON <niall.burdon@rubrash.com>
2023-12-30 11:43:57 +0100

    offfers responsive 4 refacto2

--- pages/index.vue
+++ pages/index.vue
@@ -129,26 +129,6 @@
     <h1>offer</h1>
     <section>
       <div class="container">
-        <!-- <div class="offer"> -->
-        <!-- <div class="offer__titleAndNumber">
-            <div class="offer__titleAndNumber__title">Individuelle</div>
-            <div class="number offer__titleAndNumber__number">01</div>
-          </div> -->
-        <!-- <div class="offer__description">
-            <blockquote class="offer__descriptionAndImg__description">
-              CoachKeepers propose des services d’audit, sur mesure pour les
-              structures souhaitant gagner en expertise et créer de la valeur
-              dans le domaine des gardiens de but.
-            </blockquote>
-          </div> -->
-        <!-- <div class="offer__img">
-            <img
-              src="assets/images/Coachkeepers_img3.png"
-              alt=""
-              class="offer__descriptionAndImg__image"
-            />
-          </div> -->
-        <!-- </div> -->
         <div class="offer">
           <div class="a">
             <div class="offer__titleAndNumber">

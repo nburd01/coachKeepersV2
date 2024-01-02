bc456fd730bcada33d4510ceab37b9dba5995db8
Niall BURDON <niall.burdon@rubrash.com>
2024-01-02 16:10:29 +0100

    created contactusdiv

---
+++ components/ContactUsDiv.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="contactUsDiv">
+    <div class="container">
+      <ContactUs />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,40 +15,37 @@
             le domaine des gardiens de but.
           </div>
         </div>
-        <div class="offerDiv">
-          <div class="offerDiv__offerPic"></div>
-          <div class="offerDiv__offerText">
-            <blockquote>
+      </div>
+    </section>
+    <section>
+      <div class="offerDiv">
+        <div class="offerDiv__offerPic"></div>
+        <div class="offerDiv__offerText">
+          <blockquote>
+            CoachKeepers propose des services d’audit, sur mesure pour les
+            structures souhaitant gagner en expertise et créer de la valeur dans
+            le domaine des gardiens de but.
+          </blockquote>
+          <ReadMore class="offerDiv__readMoreDiv" />
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="bannerDiv">
+        <div class="bannerDiv__content">
+          <div class="bannerDiv__content__container">
+            <div class="promise">
               CoachKeepers propose des services d’audit, sur mesure pour les
               structures souhaitant gagner en expertise et créer de la valeur
               dans le domaine des gardiens de but.
-            </blockquote>
-            <ReadMore class="offerDiv__readMoreDiv" />
+            </div>
+            <ContactUs />
           </div>
         </div>
-        <h1 class="title-main title-purple">coachkeepers</h1>
-        <h1 class="title-secondary title-info">
-          une methodologie de travail fruit de 20 ans d’experience
-        </h1>
-
-        <BubbleElement />
-        <ContactUs />
-        <h2>Nos objectifs</h2>
-        <h3>Menu</h3>
-        <blockquote>
-          CoachKeepers propose des services d’audit, sur mesure pour les
-          structures souhaitant gagner en expertise et créer de la valeur dans
-          le domaine des gardiens de but.
-        </blockquote>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at,
-          molestias nemo animi dolorem dignissimos, omnis iure alias saepe
-          quidem illum illo tempora assumenda facere voluptatibus eius iusto
-          quo? Iste!
-        </p>
-        <ReadMore />
       </div>
     </section>
+    <section></section>
+
     <section>
       <h1>Cards</h1>
       <CardElements />
@@ -59,6 +56,29 @@
     <h1>offer</h1>
     <section>
       <OfferElement />
+    </section>
+    <ContactUsDiv />
+    <section>
+      <h1 class="title-main title-purple">coachkeepers</h1>
+      <h1 class="title-secondary title-info">
+        une methodologie de travail fruit de 20 ans d’experience
+      </h1>
+
+      <BubbleElement />
+      <ContactUs />
+      <h2>Nos objectifs</h2>
+      <h3>Menu</h3>
+      <blockquote>
+        CoachKeepers propose des services d’audit, sur mesure pour les
+        structures souhaitant gagner en expertise et créer de la valeur dans le
+        domaine des gardiens de but.
+      </blockquote>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at,
+        molestias nemo animi dolorem dignissimos, omnis iure alias saepe quidem
+        illum illo tempora assumenda facere voluptatibus eius iusto quo? Iste!
+      </p>
+      <ReadMore />
     </section>
   </div>
 </template>

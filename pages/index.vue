17fe47dafd6a7543677f8c5cf8ab96c20665c066
Niall BURDON <niall.burdon@rubrash.com>
2024-01-03 10:14:24 +0100

    founder div

--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,32 @@
     <section>
       <HeaderFirst />
     </section>
-    <section>
-      <div class="container">
+    <section class="center">
+      <div class="container small">
         <div class="promiseDiv">
           <div class="bubbleDiv">
             <BubbleElement />
           </div>
           <div class="promise">
             CoachKeepers propose des services d’audit, sur mesure pour les
-            structures souhaitant gagner en expertise et créer de la valeur dans
-            le domaine des gardiens de but.
+            structures sdouhaitant gagner en expertise et créer de la valeur
+            dans le domaine des gardiens de but.
           </div>
         </div>
       </div>
     </section>
     <section>
-      <div class="offerDiv">
-        <div class="offerDiv__offerPic"></div>
-        <div class="offerDiv__offerText">
-          <blockquote>
-            CoachKeepers propose des services d’audit, sur mesure pour les
-            structures souhaitant gagner en expertise et créer de la valeur dans
-            le domaine des gardiens de but.
-          </blockquote>
-          <ReadMore class="offerDiv__readMoreDiv" />
+      <div class="container">
+        <div class="offerDiv">
+          <div class="offerDiv__container__offerPic"></div>
+          <div class="offerDiv__container__offerText">
+            <blockquote>
+              CoachKeepers propose des services d’audit, sur mesure pour les
+              structures souhaitant gagner en expertise et créer de la valeur
+              dans le domaine des gardiens de but.
+            </blockquote>
+            <ReadMore class="offerDiv__readMoreDiv" />
+          </div>
         </div>
       </div>
     </section>
@@ -46,18 +48,44 @@
     </section>
     <section></section>
 
-    <section>
+    <!-- <section>
       <h1>Cards</h1>
       <CardElements />
-    </section>
-    <h1>Pics</h1>
-    <PicsCollage />
-    <FullPagePic />
-    <h1>offer</h1>
+    </section> -->
+    <!-- <h1>Pics</h1> -->
     <section>
+      <div class="container">
+        <div class="aboutUs">
+          <div class="aboutUsDiv__promiseDiv">
+            <BubbleElement />
+            <div class="promise">Un fondateur au plus proche des terrains</div>
+          </div>
+          <div class="aboutUsDiv__block">
+            <div class="aboutUsDiv__block__offerPic">
+              <div class="aboutUsDiv__block__offerPic__1"></div>
+              <div class="aboutUsDiv__block__offerPic__2"></div>
+            </div>
+            <div class="aboutUsDiv__block__offerText">
+              <blockquote>
+                CoachKeepers propose des services d’audit, sur mesure pour les
+                structures souhaitant gagner en expertise et créer de la valeur
+                dans le domaine des gardiens de but.
+              </blockquote>
+              <ReadMore
+                class="aboutUsDiv__block__offerText__offerDiv__readMoreDiv"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <PicsCollage />
+    <!-- <FullPagePic /> -->
+    <ContactUsDiv />
+    <!-- <h1>offer</h1> -->
+    <!-- <section>
       <OfferElement />
     </section>
-    <ContactUsDiv />
     <section>
       <h1 class="title-main title-purple">coachkeepers</h1>
       <h1 class="title-secondary title-info">
@@ -79,7 +107,7 @@
         illum illo tempora assumenda facere voluptatibus eius iusto quo? Iste!
       </p>
       <ReadMore />
-    </section>
+    </section> -->
   </div>
 </template>
 
